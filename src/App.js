0175e211998bc934ae55d8c206b74fb18d02463d
Vaio <saikatbag10@gmail.com>
2022-08-01 21:48:24 +0530

    aug-1-2022

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+// import PropTypes from 'prop-types'
+import React, { Component } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from './component/About';
+import Home from './component/Home';
+import Nav from './component/Nav'
+import NewsContenar from './component/NewsContenar'
+import Saikat from './component/Saikat';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Saikat />} />
+          <Route path="/saikat" element={<Home />} />
+          <Route path="/news" element={<NewsContenar />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    )
+  }
 }
 
-export default App;
+export default App
